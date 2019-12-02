fe7c175c219eb26291142a51cca2990891be04c1
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-12-02 08:21:04 +0000

    stuck borrowing

--- src/d02.rs
+++ src/d02.rs
@@ -10,11 +10,30 @@ replace position 2 with the value 2.
 What value is left at position 0 after the program halts?
 */
 
+const ADD: u32 = 1_u32;
+const MUL: u32 = 2_u32;
+const END: u32 = 99_u32;
+
+fn step(prog: Vec<u32>, pos: usize) -> usize {
+    let opcode = prog[pos];
+    if opcode == ADD {
+        let arg1 = prog[pos + 1] as usize;
+        let arg2 = prog[pos + 2] as usize;
+        let arg3 = prog[pos + 3] as usize;
+    //prog[arg3] = prog[arg1] + prog[arg2];
+    } else if opcode == MUL {
+    } else if opcode == END {
+    } else {
+    }
+    return pos;
+}
+
 pub fn run() {
     let file_string: String = std::fs::read_to_string("input/02.txt").unwrap();
     let lines = file_string.split_terminator(',');
-    let prog: Vec<i32> = lines.map(|line| line.parse::<i32>().unwrap()).collect();
-    println!("{:#?}", prog);
+    let prog: Vec<u32> = lines.map(|line| line.parse::<u32>().unwrap()).collect();
+    //println!("{:#?}", prog);
+    step(prog, 0);
 }
 
 /*
