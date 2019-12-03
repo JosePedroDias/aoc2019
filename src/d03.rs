5916babf74c07f9ee78648664569e74021e57e7a
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-12-03 10:05:19 +0000

    03a working

--- src/d03.rs
+++ src/d03.rs
@@ -1,7 +1,45 @@
-#[derive(Debug)]
+use std::collections::HashSet;
+
+#[derive(Debug, Clone, Copy)]
 struct Step {
     direction: char,
     length: u32,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+fn manhattan_dist(x: i32, y: i32) -> u32 {
+    return (x.abs() + y.abs()) as u32;
+}
+
+fn expand_line(steps: &Vec<Step>) -> Vec<Coord> {
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut vec = Vec::new();
+    //vec.push(Coord { x: x, y: y });
+
+    for step in steps.iter() {
+        for _ in 0..step.length {
+            let dx: i32 = match step.direction {
+                'L' => -1,
+                'R' => 1,
+                _ => 0,
+            };
+            let dy: i32 = match step.direction {
+                'U' => -1,
+                'D' => 1,
+                _ => 0,
+            };
+            x += dx;
+            y += dy;
+            vec.push(Coord { x: x, y: y });
+        }
+    }
+    return vec;
 }
 
 pub fn run() {
@@ -19,8 +57,48 @@ pub fn run() {
                 .collect()
         })
         .collect();
-    println!("03a: {:#?}", lines);
+
+    //println!("03a: {:#?}", lines);
+
+    let mut shortest_dist: u32 = 100000; //u32.MAX;
+
+    let mut set: HashSet<Coord> = HashSet::new();
+    for line in lines.iter() {
+        let coords: Vec<Coord> = expand_line(&line);
+        for coord in coords.iter() {
+            if set.contains(coord) {
+                let dist = manhattan_dist(coord.x, coord.y);
+                if dist < shortest_dist {
+                    shortest_dist = dist;
+                }
+            }
+            set.insert(*coord);
+        }
+    }
+
+    println!("03a: {:#?}", shortest_dist);
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expand_line_1() {
+        let steps = [Step {
+            direction: 'D',
+            length: 2,
+        }]
+        .to_vec();
+        let coords = expand_line(&steps);
+        assert_eq!(
+            coords,
+            [
+                //Coord { x: 0, y: 0 },
+                Coord { x: 0, y: 1 },
+                Coord { x: 0, y: 2 }
+            ]
+            .to_vec()
+        );
+    }
+}
