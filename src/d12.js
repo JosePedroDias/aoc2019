4ca551bfb38dd0381ccfbba97ed17c32e0fcae5c
josepedrodiassky <jose.dias@sky.uk>
2019-12-12 10:11:08 +0000

    wip 12a

--- src/d12.js
+++ src/d12.js
@@ -1,7 +1,36 @@
 const fs = require('fs');
 const assert = require('assert');
 
+/*
+apply gravity to update velocity (for every pair of entities and each coord, -1, 0 or 1 by comparison)
+apply velocity to update position
+*/
+
 const RGX = /<x=(-?\d+), y=(-?\d+), z=(-?\d+)>/;
+
+// order being irrelevant
+function combinations(n) {
+  const arr = [];
+  for (let i = 0, j; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      if (i < j) {
+        arr.push([i, j]);
+      }
+    }
+  }
+  return arr;
+}
+
+function step() {}
+
+function ent(pos) {
+  return {
+    pos,
+    vel: [0, 0, 0]
+  };
+}
+
+function energy() {}
 
 function main() {
   const file = fs.readFileSync('input/12.txt').toString();
@@ -14,7 +43,20 @@ function main() {
 
 function test() {
   (() => {
-    assert.equal(true, true);
+    assert.deepEqual(combinations(2), [[0, 1]]);
+    assert.deepEqual(combinations(3), [
+      [0, 1],
+      [0, 2],
+      [1, 2]
+    ]);
+    assert.deepEqual(combinations(4), [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+      [1, 2],
+      [1, 3],
+      [2, 3]
+    ]);
   })();
 }
 
