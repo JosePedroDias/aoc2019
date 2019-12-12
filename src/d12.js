d212cfab3bf9ed3783ad0c994c41f5a94239119c
josepedrodiassky <jose.dias@sky.uk>
2019-12-12 09:55:42 +0000

    parse 12 input

--- src/d12.js
+++ src/d12.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const assert = require('assert');
 
+const RGX = /<x=(-?\d+), y=(-?\d+), z=(-?\d+)>/;
+
 function main() {
   const file = fs.readFileSync('input/12.txt').toString();
-  const lines = file.split('\n');
+  const lines = file.split('\n').map((s) => {
+    const m = RGX.exec(s);
+    return [m[1], m[2], m[3]].map(parseFloat);
+  });
+  console.log(lines);
 }
 
 function test() {
