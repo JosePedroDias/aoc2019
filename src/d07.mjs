9001758a9d711b842d62397b2f6ab6ac040f3175
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-12-17 22:50:33 +0000

    07a failing

--- src/d07.mjs
+++ src/d07.mjs
@@ -1,9 +1,16 @@
 import fs from 'fs';
 import assert from 'assert';
-import { runProgram } from './intcode';
+import { runProgram } from './intcode.mjs';
+import { permutations5 } from './utils.mjs';
 
 function run(prog, a, b, c, d, e) {
-  let iA = undefined;
+  let incA = 0;
+  let incB = 0;
+  let incC = 0;
+  let incD = 0;
+  let incE = 0;
+
+  let iA = a;
   let iB = b;
   let iC = c;
   let iD = d;
@@ -15,68 +22,155 @@ function run(prog, a, b, c, d, e) {
   let oD = undefined;
   let oE = undefined;
 
-  function inA() {
-    const v = iA;
-    ia = 0;
-    return v;
-  }
-  function inB() {
-    return iB;
-  }
-  function inC() {
-    return iC;
-  }
-  function inD() {
-    return iD;
-  }
-  function inE() {
-    return iE;
-  }
-
-  function outA(v) {
-    oA = v;
-  }
-  function outB(v) {
-    oB = v;
-  }
-  function outC(v) {
-    oC = v;
-  }
-  function outD(v) {
-    oD = v;
-  }
-  function outE(v) {
-    oE = v;
-  }
-
   // a, 0....
   // b, outA...
   // c, outB...
   // d, outC...
   // e?
 
-  runProgram(prog, inA, outA);
-  runProgram(prog, inB, outB);
-  runProgram(prog, inC, outC);
-  runProgram(prog, inD, outE);
-  runProgram(prog, inE, outE);
+  runProgram(
+    prog.slice(),
+    () => {
+      const v = iA;
+      if (!incA) {
+        ++incA;
+        iA = 0;
+      }
+      return v;
+    },
+    (v) => {
+      oA = v;
+    }
+  );
 
-  console.log(oE);
+  runProgram(
+    prog.slice(),
+    () => {
+      const v = iB;
+      if (!incB) {
+        ++incB;
+        iB = oA;
+      }
+      return v;
+    },
+    (v) => {
+      oB = v;
+    }
+  );
+
+  runProgram(
+    prog.slice(),
+    () => {
+      const v = iC;
+      if (!incC) {
+        ++incC;
+        iC = oB;
+      }
+      return v;
+    },
+    (v) => {
+      oC = v;
+    }
+  );
+
+  runProgram(
+    prog.slice(),
+    () => {
+      const v = iD;
+      if (!incD) {
+        ++incD;
+        iD = oC;
+      }
+      return v;
+    },
+    (v) => {
+      oD = v;
+    }
+  );
+
+  runProgram(
+    prog.slice(),
+    () => {
+      const v = iE;
+      if (!incE) {
+        ++incE;
+        iE = oD;
+      }
+      return v;
+    },
+    (v) => {
+      oE = v;
+    }
+  );
 
   return oE;
 }
 
+function runTestProgram(s, a, b, c, d, e) {
+  const lines = s.split(',');
+  const prog = lines.map((v) => parseInt(v, 10));
+  return run(prog, 4, 3, 2, 1, 0);
+}
+
 function main() {
-  const file = fs.readFileSync('input/07.txt').toString();
-  const lines = file.split(',');
+  const s = fs.readFileSync('input/07.txt').toString();
+  const lines = s.split(',');
   const prog = lines.map((v) => parseInt(v, 10));
 
-  console.log('07a:');
-  run(prog, 4, 3, 2, 1, 0);
+  let max = 0;
+  const inputs = permutations5(5);
+  for (let [a, b, c, d, e] of inputs) {
+    const v = run(prog, 4, 3, 2, 1, 0);
+    if (v > max) {
+      max = v;
+    }
+  }
+  console.log('07a:', max);
 }
 
 function test() {
-  (() => {})();
+  (() => {
+    assert.equal(
+      runTestProgram(
+        '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0',
+        4,
+        3,
+        2,
+        1,
+        0
+      ),
+      43210
+    );
+  })();
+
+  (() => {
+    assert.equal(
+      runTestProgram(
+        '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0',
+        0,
+        1,
+        2,
+        3,
+        4
+      ),
+      12345
+    );
+  })();
+
+  (() => {
+    assert.equal(
+      runTestProgram(
+        '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0',
+        1,
+        0,
+        4,
+        3,
+        2
+      ),
+      21065
+      //65210 FAILING
+    );
+  })();
 }
 
 test();
