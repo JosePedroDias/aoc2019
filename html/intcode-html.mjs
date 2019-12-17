1f3d9c7a6c627fb57c29ed11147a56ae7c7600b3
josepedrodiassky <jose.dias@sky.uk>
2019-12-17 14:37:21 +0000

    better layout

--- html/intcode-html.mjs
+++ html/intcode-html.mjs
@@ -1,7 +1,5 @@
 import { stepProgram, log } from '../src/intcode.mjs';
 
-const STEP_MS = 25;
-
 function q(s) {
   return document.querySelector(s);
 }
@@ -10,7 +8,8 @@ function parse(s) {
   return s.split(',').map((t) => parseInt(t, 10));
 }
 
-const inputEl = q('input');
+const fileEl = q('.file');
+const stepMsEl = q('.step_ms');
 const stepEl = q('.step');
 const runEl = q('.run');
 const memoryEl = q('.memory');
@@ -23,15 +22,14 @@ const statEls = {
   c: q('.c')
 };
 
-let p, cells;
+let p, cells, repeatInput;
 
-let repeatInput = undefined;
 function getInput() {
   if (repeatInput) {
     return repeatInput;
   }
 
-  let v = prompt('value?', 0);
+  let v = prompt('Value? (end input with excl mark to keep providing this)', 0);
   if (v[v.length - 1] === '!') {
     repeatInput = v;
   }
@@ -76,20 +74,21 @@ function step() {
 }
 
 function run() {
+  const stepMs = stepMsEl.value;
   let t;
   function runStep() {
     if (!step()) {
       clearInterval(t);
     }
   }
-  t = setInterval(runStep, STEP_MS);
+  t = setInterval(runStep, stepMs);
 }
 
 function process(data_) {
   const data = parse(data_);
 
+  repeatInput = undefined;
   cells = [];
-
   p = {
     index: 0,
     cells: data
@@ -122,6 +121,6 @@ function onFile(ev) {
   reader.readAsText(f);
 }
 
-inputEl.addEventListener('change', onFile);
+fileEl.addEventListener('change', onFile);
 stepEl.addEventListener('click', step);
 runEl.addEventListener('click', run);
